afb4446e8526dcb63893320b898c88c1e3200275
umenosuke <umenosuke.net@gmail.com>
2020-04-09 13:56:25 +0900

    変数名を修正

--- show_cert.sh
+++ show_cert.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-_CLIENT_CRT=${1}
-if [ -z ${_CLIENT_CRT} ]; then
+_CRT_PATH=${1}
+if [ -z ${_CRT_PATH} ]; then
   exit 1
 fi
-_ABS_CLIENT_CRT="$(readlink -f ${_CLIENT_CRT})"
+_ABS_CRT_PATH="$(readlink -f ${_CRT_PATH})"
 
 cd "$(dirname $(readlink -f $0))/"
 _BASE_DIR="$(pwd)"
 
-cat "${_ABS_CLIENT_CRT}" | openssl x509 -noout -text | less
+cat "${_ABS_CRT_PATH}" | openssl x509 -noout -text | less
