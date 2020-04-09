c56ecefb4401d9502e85b98951c366cdd8b9b39e
umenosuke <umenosuke.net@gmail.com>
2020-04-09 13:49:44 +0900

    show certを別パスからの実行可能に

--- show_cert.sh
+++ show_cert.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd $(dirname $0)
-
-CLIENT_CRT=${1}
-if [ -z ${CLIENT_CRT} ]; then
+_CLIENT_CRT=${1}
+if [ -z ${_CLIENT_CRT} ]; then
   exit 1
 fi
+_ABS_CLIENT_CRT="$(readlink -f ${_CLIENT_CRT})"
 
-cat "${CLIENT_CRT}" | openssl x509 -noout -text | less
+cd "$(dirname $(readlink -f $0))/../"
+_BASE_DIR="$(pwd)"
+
+cat "${_ABS_CLIENT_CRT}" | openssl x509 -noout -text | less
